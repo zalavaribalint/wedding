75faeffaeac05738a26637199af6dde30382b596
Zalavári Bálint <balint.zalavari@suitsolutions.eu>
2022-10-31 13:05:55 +0100

    add centerAlign property to the simple block component

--- src/app/simple-block/simple-block.component.ts
+++ src/app/simple-block/simple-block.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-simple-block',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./simple-block.component.scss']
 })
 export class SimpleBlockComponent implements OnInit {
+
+  @Input() centerAlign = true;
 
   constructor() { }
 
