c8dff1e1a87e3b8c9606514fdabd8d195874e196
Zalavári Bálint <balint.zalavari@suitsolutions.eu>
2022-10-31 13:05:40 +0100

    add centerAlign property to the split block component

--- src/app/split-block/split-block.component.ts
+++ src/app/split-block/split-block.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-split-block',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./split-block.component.scss']
 })
 export class SplitBlockComponent implements OnInit {
+
+  @Input() centerAlign = true;
 
   constructor() { }
 
