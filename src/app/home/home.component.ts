f4a4e5c320f1c71d13518380391b408dcdf18796
Zalavári Bálint <balint.zalavari@suitsolutions.eu>
2022-11-01 11:41:42 +0100

    shuffle the images in the slider on the home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -85,6 +85,25 @@ export class HomeComponent implements OnInit {
   ];
 
   ngOnInit(): void {
+    this.imageObject = this.shuffle(this.imageObject);
   }
+
+  shuffle<T>(array: T[]): T[] {
+    let currentIndex = array.length,  randomIndex;
+
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+
+    return array;
+  };
 
 }
