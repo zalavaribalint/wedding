b7a0d58cda21a5b278261bca2aee36d203c61ace
Bence Nagy <nagy.bence1337@gmail.com>
2022-11-21 17:16:59 +0100

    feat(home): render image slider only if visible

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,10 +5,11 @@ import { NgImageSliderModule } from 'ng-image-slider';
 import { CommonModule } from '@angular/common';
 import { SimpleBlockModule } from '@shared/components/simple-block';
 import { SplitBlockModule } from '@shared/components/split-block';
+import { DirectivesModule } from '@shared/directives';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, TranslateModule, NgImageSliderModule, SimpleBlockModule, SplitBlockModule],
+  imports: [CommonModule, TranslateModule, NgImageSliderModule, SimpleBlockModule, SplitBlockModule, DirectivesModule],
   exports: [HomeComponent],
 })
 export class HomeModule {}
