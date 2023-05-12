d23f4d72443466e075d6cdf703a31cc648b88580
Zalavári Bálint <balint.zalavari@suitsolutions.eu>
2023-05-12 15:00:08 +0200

    feat: add content to the gallery component

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,10 +1,9 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
-  styleUrls: ['./gallery.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrls: ['./gallery.component.scss']
 })
 export class GalleryComponent {
 
--- src/app/gallery/gallery.module.ts
+++ src/app/gallery/gallery.module.ts
@@ -6,9 +6,11 @@ import { SimpleBlockModule } from '@shared/components/simple-block';
 import { SplitBlockModule } from '@shared/components/split-block';
 import { DirectivesModule } from '@shared/directives';
 import { GalleryRoutingModule } from './gallery-routing.module';
+import {Angular2ImageGalleryModule} from "angular2-image-gallery";
+import {HammerModule} from "@angular/platform-browser";
 
 @NgModule({
   declarations: [GalleryComponent],
-  imports: [CommonModule, GalleryRoutingModule, TranslateModule,  SimpleBlockModule, SplitBlockModule, DirectivesModule]
+  imports: [CommonModule, GalleryRoutingModule, TranslateModule,  SimpleBlockModule, SplitBlockModule, DirectivesModule, Angular2ImageGalleryModule, HammerModule]
 })
 export class GalleryModule {}
